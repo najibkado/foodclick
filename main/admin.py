42bc1ffde3b8510ab9d47fabedfd798d11640c93
najibkado <dopeland3@gmail.com>
2021-11-20 12:02:54 +0100

    Landing page update

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Entity, Listing, Giveaway
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Entity)
+admin.site.register(Listing)
+admin.site.register(Giveaway)
