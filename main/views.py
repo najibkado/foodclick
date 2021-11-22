0b2da258c768a71083158936d512623fa0dd82d1
najibkado <dopeland3@gmail.com>
2021-11-22 11:39:58 +0100

    resolved listing return issue

--- main/views.py
+++ main/views.py
@@ -137,8 +137,8 @@ class ListingsApiView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def get(self, request):
-        data = Entity.objects.filter(user=request.user)
+    def get(self):
+        data = Listing.objects.all()
         serializer = ListingSerializer(data, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
